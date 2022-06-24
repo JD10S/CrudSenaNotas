63aa21203ba040ba59d90fcabf9cfc03642f4c2c
Juan David Castañeda Betancourt <jdcb30112005@gmail.com>
2022-06-23 20:54:14 -0500

    Router v2

--- Core/Router.php
+++ Core/Router.php
@@ -4,7 +4,10 @@ class Router {
     protected $routes = [];
     protected $params = [];
 
-    public function add($route, $params){
+    public function add($route, $params = []){
+        $route = preg_replace('/\//', '\\/', $route);
+        $route = preg_replace('/\{([a-z]+)\}/','(?P<\1>[a-z-]+)',$route);
+        $route = '/^'.$route.'$/i';
         $this->routes[$route] = $params;
     }
     public function getRoutes(){
@@ -12,13 +15,19 @@ class Router {
     }
     public function match($url)
     {
-        foreach ($this->routes as $route => $params) {
-            if ($url == $route) {
-                $this->params = $params;
-                return true; 
+        foreach($this->routes as $route => $params){
+            if(preg_match($route,$url,$matches)) 
+            {
+                foreach($matches as $key => $match){
+                    if(is_string($key)){
+                        $params[$key] = $match;
+                    }
+                }
+                $this->params = $params; 
+                return true;    
             }
         }
-        return false;
+    return false;
     }
     public function getParams(){
         return $this->params; 
--- Public/index.php
+++ Public/index.php
@@ -2,14 +2,16 @@
 //Definimos el Router
 require '../Core/Router.php';
 $router = new Router();
-//añadiendo rutas
+//añadiendo rutas utiliza '{}' para definir una variable. 
 $router->add('',['controller'=>'Home', 'action' => 'index']);
 $router->add('Hola',['controller'=>'Home', 'action' => 'saludo']);
 $router->add('fuaeldiego',['controller'=>'xD', 'action' => 'fua']);
 $router->add('fuaeldiego/diegol',['controller'=>'xD', 'action' => 'fua a casa ingle']);
+$router->add('siuuu/{nose}');
+$router->add('{nose}/{talvez}');
 
-//Definimos url y la comparamos con las rutas
-$url = $_SERVER['QUERY_STRING'];
+//Definimos url y la enviamos a la funcion match y esta abastraiga metodo y controlador
+$url = strtolower($_SERVER['QUERY_STRING']);
 
 if($router->match($url)){
     echo '<pre>';
